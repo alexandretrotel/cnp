2d83c72d95782f8b6642e5a462891e091859f590
Alexandre Trotel <trotelalexandre@proton.me>
2025-01-23 09:50:50 +0100

    fix: deploy

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,7 @@ cargo build --release
 # Copy the executable to /usr/local/bin
 sudo cp target/release/cnp /usr/local/bin/
 
+# Add permissions to execute the program
+sudo chmod +x /usr/local/bin/cnp
+
 echo "Deployment complete!"
